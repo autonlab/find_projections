8fa67c9b65b1838e251da5ae92fb4626807f6dfe
Saswati Ray <sray@cs.cmu.edu>
2018-01-16 15:01:35 -0500

    Code restructuring according to new API (2018 Jan 5) for D3M.

--- find_projections/__init__.py
+++ find_projections/__init__.py
@@ -1,1 +1,3 @@
-from .search_projections import Search
+from .search_projections import Search, SearchHyperparams
+from .search_numeric_projections import SearchNumeric, SearchNumericHyperparams
+from .feature_map import FeatureMap

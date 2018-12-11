cfd469f6e1fabba057e04c8865d51a5af24394e5
Saswati Ray <sray@cs.cmu.edu>
2018-12-11 10:27:33 -0500

    Update test program

--- test_projections.py
+++ test_projections.py
@@ -2,6 +2,7 @@
 import sys, csv
 import numpy
 from sklearn import preprocessing
+import pandas as pd
 
 #from d3m.primitives.cmu.autonlab.find_projections import Search, SearchNumeric
 
@@ -10,16 +11,18 @@ import find_projections.search_numeric_projections as search_numeric_projections
 
 # Read input feature set
 result = numpy.random.rand(1000,2).astype("float")
+rf = pd.DataFrame(data=result, columns=['x', 'y'])
 
 # CLASSIFICATION
 # Read output feature for classification
 output = numpy.random.randint(2, size=(1000,)).astype("float")
+of = pd.DataFrame(data=output, columns=['Label'])
 
 hyperparams = search_projections.SearchHyperparams(purity=0.75, binsize=10, support=25, num_threads=1, validation_size=0.1)
 # Create search object and parameters
 search_object = search_projections.Search(hyperparams=hyperparams)
 
-search_object.set_training_data(inputs=result, outputs=output)
+search_object.set_training_data(inputs=rf, outputs=of)
 
 # Search comprehensively for projection boxes
 fmap = search_object.search_projections()
@@ -33,8 +36,7 @@ for i in range(num):
 # Search for easy-to-classify data (decision list)
 search_object.fit()
 
-predicted = search_object.produce(inputs=result).value
-print(predicted)
+predicted = search_object.produce(inputs=rf).value
 
 fmap = search_object.get_feature_map()
 num = fmap.get_num_projections()
@@ -48,12 +50,11 @@ for i in range(num):
 hyperparams = search_numeric_projections.SearchNumericHyperparams(binsize=10, support=25, mode=1, num_threads=1, validation_size=0.1)
 # Create search object and parameters
 search_object = search_numeric_projections.SearchNumeric(hyperparams=hyperparams)
-search_object.set_training_data(inputs=result, outputs=output)
+search_object.set_training_data(inputs=rf, outputs=of)
 
 # Search or easy-to-regress data (decision list)
 search_object.fit()
-predicted = search_object.produce(inputs=result).value
-print(predicted)
+predicted = search_object.produce(inputs=rf).value
 
 fmap = search_object.get_feature_map()
 num = fmap.get_num_projections()

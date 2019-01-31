480b70b423d144ec8a90c0d1a999874caaaf6840
Donghan Wang <donghanw@cs.cmu.edu>
2019-01-31 13:52:32 -0500

    use the mutli_prodcue() provided in the base class

--- find_projections/search_numeric_projections.py
+++ find_projections/search_numeric_projections.py
@@ -226,12 +226,3 @@ class SearchNumeric(SupervisedLearnerPrimitiveBase[Input, Output, SearchNumericP
 
          output = container.DataFrame(predictedTargets, generate_metadata=False, source=self)
          return base.CallResult(output)
-
-     def multi_produce(self, *, produce_methods: typing.Sequence[str], inputs: Input, timeout: float = None,
-                       iterations: int = None) -> base.MultiCallResult:
-         output = self.produce(inputs=inputs)
-         result = {}
-         for method in produce_methods:
-             result[method] = output.value
-         return base.MultiCallResult(result)
-

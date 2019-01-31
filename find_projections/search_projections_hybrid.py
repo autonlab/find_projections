7f29a4626048f15492662848fde4e968d74aec97
Donghan Wang <donghanw@cs.cmu.edu>
2019-01-31 12:44:16 -0500

    use the mutli_prodcue() provided in the base class

--- find_projections/search_projections_hybrid.py
+++ find_projections/search_projections_hybrid.py
@@ -262,11 +262,3 @@ class SearchHybrid(SupervisedLearnerPrimitiveBase[Input, Output, SearchHybridPar
          predictedTargetNames = self._le.inverse_transform(predictedTargets)
          output = container.DataFrame(predictedTargetNames, generate_metadata=False, source=self)
          return base.CallResult(output)
-
-     def multi_produce(self, *, produce_methods: typing.Sequence[str], inputs: Input, timeout: float = None,
-                       iterations: int = None) -> base.MultiCallResult:
-         output = self.produce(inputs=inputs)
-         result = {}
-         for method in produce_methods:
-             result[method] = output.value
-         return base.MultiCallResult(result)
